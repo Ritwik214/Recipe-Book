b13dbca49dc526ee0df6269bef9f1bf3939666b4
Ritwik Kumar Gupta <ritwikkumar111@gmail.com>
2023-07-27 14:31:14 +0530

    first commit

--- single-recipe.js
+++ single-recipe.js
@@ -1,5 +1,6 @@
-const cardId = localStorage.getItem("id");
+// single-recipe.js
 
+const cardId = localStorage.getItem("id");
 const SINGLERECIPEURL = `https://recipeapi.prakashsakari.repl.co/api/recipes/${cardId}`;
 
 const getData = async (URL) => {
@@ -11,5 +12,43 @@ const getData = async (URL) => {
   }
 };
 
-const singleRecipe = await getData(SINGLERECIPEURL);
-console.log(singleRecipe);
+// Function to update the HTML elements with the fetched data
+const updateRecipeData = (recipeData) => {
+  console.log(recipeData); // Check the entire object in the browser console
+
+  const recipeTitleElement = document.getElementById('recipe-title');
+  const recipeImageElement = document.getElementById('recipe-image');
+  const recipeIngredientsElement = document.getElementById('recipe-ingredients');
+  const recipeInstructionsElement = document.getElementById('recipe-instructions');
+
+  if (recipeData && recipeData.length > 0) {
+    const recipe = recipeData[0]; // Access the first recipe object in the array
+
+    recipeTitleElement.textContent = recipe.TranslatedRecipeName;
+    recipeImageElement.src = recipe['image-url'];
+    recipeIngredientsElement.textContent = recipe.TranslatedIngredients;
+    recipeInstructionsElement.textContent = recipe.TranslatedInstructions;
+  }
+};
+
+// Debounce function
+function debounce(func, delay) {
+  let timer;
+
+  return function (...args) {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
+// Debounced API call function
+const debouncedFetchData = debounce(async () => {
+  const singleRecipe = await getData(SINGLERECIPEURL);
+  updateRecipeData(singleRecipe);
+}, 500); // Adjust the delay as needed (500ms in this case)
+
+// Initial call
+debouncedFetchData();
